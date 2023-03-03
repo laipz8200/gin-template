10163d4b97cff415274e7db4358da2e0c5e4a442
-LAN- <laipz8200@outlook.com>
2023-03-04 02:38:59 +0800

    Add Atomic method example

--- domain/repository.go
+++ domain/repository.go
@@ -1,4 +1,7 @@
 package domain
 
+import "context"
+
 type IRepository interface {
+	Atomic(context.Context, func(IRepository) error) error
 }
--- infrastructure/datastore/repository.go
+++ infrastructure/datastore/repository.go
@@ -1,7 +1,28 @@
 package datastore
 
-import "_template_/domain"
+import (
+	"_template_/domain"
+	"context"
+
+	"gorm.io/gorm"
+)
 
 var _ domain.IRepository = (*repository)(nil)
 
-type repository struct{}
+type repository struct {
+	db *gorm.DB
+}
+
+// Atomic implements domain.IRepository
+func (r *repository) Atomic(ctx context.Context, fn func(domain.IRepository) error) error {
+	return r.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		repo := NewRepository(tx)
+		return fn(repo)
+	})
+}
+
+func NewRepository(db *gorm.DB) domain.IRepository {
+	return &repository{
+		db: db,
+	}
+}
