f27fe78eea5e5f951f15d6c7f7557b1a67266e36
-LAN- <laipz8200@outlook.com>
2023-03-17 11:36:58 +0800

    Update config: Remove comments.

--- config/config.go
+++ config/config.go
@@ -8,14 +8,11 @@ import (
 var c config = config{}
 
 func Init(filepath string) {
-	// read our opened xmlFile as a byte array.
 	byteValue, err := os.ReadFile(filepath)
 	if err != nil {
 		panic(err)
 	}
 
-	// unmarshal our byteArray which contains our
-	// jsonFile's content into 'config' which we defined above
 	if err := json.Unmarshal(byteValue, &c); err != nil {
 		panic(err)
 	}
